0fb861d27145dbc578c03779bb1d103ea76bcb72
bestboom <certseek@gmail.com>
2019-02-04 20:14:00 +0500

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-const callmade = require('.env');
-const privateKey = process.env.callmade.my_key;
-=======
+const callmade = require('/Users/Abdul/Documents/GitHub/mymoon/.env');
+(function () {
+    console.log(callmade.my_key);
 
-const callmade = require('.env');
-const privateKey = callmade.my_key;
->>>>>>> 553992199fea46011935b517b24286c3b18c279b
-console.log(privateKey);
+})();
