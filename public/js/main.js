05bdaa6a420d10849b0b1a56b7b6bab82c3c37bf
bestboom <certseek@gmail.com>
2019-02-04 20:49:24 +0500

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-const callmade = require('/Users/Abdul/Documents/GitHub/mymoon/.env');
-=======
-const callmade = require('.env');
-(function () {
-    console.log(callmade.my_key);
->>>>>>> 80d163c94ea19bd9aebade9e921e20c7322ce98c
-
-const privateKey = callmade.my_key;
+var dotenv = require('dotenv');
+dotenv.load();
+const privateKey = process.env.my_key;
 console.log(privateKey);
